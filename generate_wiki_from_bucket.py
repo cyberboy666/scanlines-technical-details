be7cca68ce54e22288cc4e4a5aadf59218baed6a
langolierz <langolierz@gmail.com>
2020-09-22 19:02:12 +0200

    Update generate_wiki_from_bucket.py

--- generate_wiki_from_bucket.py
+++ generate_wiki_from_bucket.py
@@ -8,28 +8,66 @@ then run it while setting the access_key and secret: AWS_ACCESS_KEY_ID=XXX AWS_S
 
 import boto3
 import urllib.parse
+import os
 
 s3 = boto3.resource('s3', endpoint_url='https://s3.eu-central-1.wasabisys.com')
-
-
 my_bucket = s3.Bucket('scanlines-other')
 
-with open("Output.txt", "w") as text_file:
-    text_file.write("# DIY RESOURCES\n")
+# generate the file system recusively
+file_system = []
 
-    for index, my_bucket_object in enumerate(my_bucket.objects.all()):
-        # print(my_bucket_object.key.split('/'))
-        folder_split = my_bucket_object.key.split('/')
-        if folder_split[-1] == '':
-            if index > 0:
-                text_file.write('</details>')
-            path = '/'.join(folder_split[1:-1])
-            print('Header: ', path)
-            text_file.write(f"\n<details><summary>{path}</summary>\n\n")
-        elif '.' in folder_split[-1]:
-            path = '/'.join(folder_split[2:])
-            text_file.write(f" - [{path}](https://s3.eu-central-1.wasabisys.com/scanlines-other/{urllib.parse.quote(my_bucket_object.key)})\n")
-            print('File: ', '/'.join(folder_split[1:]))
+def dict_at_this_slice(current_subsystem, slice_list):
+    # find the subsystem of the file system by searching through with a slice list
+    if len(slice_list) == 0 or len(current_subsystem) == 0:
+        # if at end of the list or empty then return
+        return current_subsystem
+    # the next level is found based on first item in the slice list
+    next_subsystem = [item[1] for item in current_subsystem if item[0] == slice_list[0]][0]
+    if len(slice_list) == 1:
+        # if we are now at the end of slice list then return this subsystem
+        return next_subsystem
+    else:
+        # otherwise go deeper into in as per the slice list
+        return dict_at_this_slice(next_subsystem, slice_list[1:])
+
+def add_to_this_folder(path_beginning, path):
+    # use the path_beginning list to seek to current subsystem
+    current_depth = dict_at_this_slice(file_system, path_beginning)
+    if len(path) == 1:
+        # if end of path list then it is a file. add this file to the filesystem
+        if path[0]:
+            current_depth.append(('file', f'<a target="_blank" href="https://s3.eu-central-1.wasabisys.com/scanlines-other/{urllib.parse.quote(my_bucket_object.key)}">{path[0]}</a>'))
+    else:
+        # if not end of path list then it is a folder, check if the folder is already in the file system:
+        if not any(True for item in current_depth if item[0] == path[0]):
+            # if the folder is not in the file system add it now
+            current_depth.append((path[0], []))
+
+        # this folder is added to the path_beggining and we enter next the recursive layer (for the next item in after this folder in the path list)
+        path_beginning.append(path[0])
+        add_to_this_folder(path_beginning, path[1:])
+
+def convert_object_to_markdown(file_system):
+    for item in file_system:
+        if item[0] == 'file':
+            text_file.write(f'<li>{item[1]}</li>\n')
         else:
-            print('dunno what: ', folder_split)
-    text_file.write('</details>')
+            text_file.write(f'<li><details><summary>{item[0]}</summary><ul>')
+            convert_object_to_markdown(item[1])
+            text_file.write(f'</ul></details></li>\n')
+
+all_objects = [obj for obj in my_bucket.objects.all()]
+
+for my_bucket_object in all_objects:
+    path = my_bucket_object.key.split('/')
+    add_to_this_folder([], path)
+print('file_system: ', file_system)
+# jump into the first folder (diy resources)
+file_system = file_system[0][1]
+
+with open("Output.txt", "w") as text_file:
+    text_file.write(f'# DIY RESOURCES\n\n')
+    text_file.write("<ul>")
+    convert_object_to_markdown(file_system)
+    text_file.write("</ul>")
+
